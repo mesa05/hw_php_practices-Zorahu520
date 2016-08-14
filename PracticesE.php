34955c6a67fd4ffe2af62309b91afa9a66e45375
GitLabUser <gitlabst@gmail.com>
2016-08-14 08:27:25 +0000

    PracticesE done

--- PracticesE.php
+++ PracticesE.php
@@ -6,6 +6,44 @@ E. 輸入一個句子，分別統計句中各字母與數字出現的次數。
 
 header("content-type: text/html; charset=utf-8");
 
+$str = $_POST["txtStr"];
+$strLen = strlen($str);
+$arrLetter = [];
+$arrNumber = [];
 
+for( $i = 0 ; $i <= $strLen ; $i++){
+        $aLetLen = count($arrLetter);
+        for( $j=0 ; $j <= $aLetLen ; $j++){
+            if($arrLetter[$j] == substr($str,$i,1)){
+            $arrNumber[$j] += 1;
+            break;
+            }
+            else if($arrLetter[$j] == ""){
+            $arrLetter[$j] = substr($str,$i,1);
+            $arrNumber[$j] += 1;
+            break;
+            }
+        }
+    }
+
+echo $str;
+echo "<hr>";
+    if(isset($_POST["bntSubmit"])){
+        for ($answer = 0; $answer < $aLetLen ; $answer++) {
+	        echo $arrLetter[$answer]." =>" ."&nbsp".$arrNumber[$answer]."&nbsp"."次"."<br />";
+        }
+    }
 
 ?>
+
+
+<html>
+    <head>
+    </head>
+    <body>
+        <form method="post">
+        輸入一個句子:<input type="text" name="txtStr">
+        <input type="submit" name="bntSubmit" value="submit">
+        </form>
+    </body>
+</html>
